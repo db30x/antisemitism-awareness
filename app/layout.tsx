fe51825bf1e675619f6b9c751bfc7d45c0b19dda
3dbalin <dbalin@uci.edu>
2025-03-08 18:25:25 -0800

    changed icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,12 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'AntisemitismExists',
   description: 'Raising awareness about antisemitism in our digital world',
+  icons: {
+    icon: [
+      { url: '/sixteen.png', sizes: '16x16', type: 'image/png' },
+      { url: '/thirtytwo.png', sizes: '32x32', type: 'image/png' }
+    ]
+  }
 }
 
 export default function RootLayout({
